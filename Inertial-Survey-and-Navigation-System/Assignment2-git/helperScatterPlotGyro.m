c614a96f7c6703e8460b8759a9d403c8a0d9be5d
chinchia120 <chinchia120@gmail.com>
2024-05-06 17:36:10 +0800

    update

--- Inertial-Survey-and-Navigation-System/Assignment2-git/helperScatterPlotGyro.m
+++ Inertial-Survey-and-Navigation-System/Assignment2-git/helperScatterPlotGyro.m
@@ -1,13 +1,19 @@
 function [] = helperScatterPlotGyro(IMUData, OutputDir)
     scatter(IMUData(:, 1), IMUData(:, 2: end), 'filled');
-    title('Gyroscope and Time', 'FontSize', 16);
-    xlabel("time (s)", 'FontSize', 14);
-    ylabel("Gyro value (rad/s)", 'FontSize', 14);
-    legend('Gyro-X', 'Gyro-Y', 'Gyro-Z', 'FontSize', 14);
+    title('Gyroscope and Time', 'FontSize', 32);
+    xlabel("time (s)", 'FontSize', 24);
+    ylabel("Gyro value (rad/s)", 'FontSize', 24);
+
+    if size(IMUData, 2) == 2
+        legend('Gyro-Z', 'FontSize', 24);
+    else
+        legend('Gyro-X', 'Gyro-Y', 'Gyro-Z', 'FontSize', 24);
+    end
 
     Pix_SS = get(0, 'screensize');
     width = 1750; height = 875;
     set(gcf, 'position', [(Pix_SS(3)-width)/2, (Pix_SS(4)-height)/2, width, height]);
-    saveas(gcf, OutputDir);
+    % saveas(gcf, [OutputDir, '.fig']);
+    saveas(gcf, [OutputDir, '.png']);
 end
 
