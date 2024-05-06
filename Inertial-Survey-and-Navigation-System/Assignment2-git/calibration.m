eab2745af9492584797fe40d0016025f22fbb500
chinchia120 <chinchia120@gmail.com>
2024-05-06 17:36:10 +0800

    update

--- Inertial-Survey-and-Navigation-System/Assignment2-git/calibration.m
+++ Inertial-Survey-and-Navigation-System/Assignment2-git/calibration.m
@@ -13,38 +13,46 @@ end
 % openExample('shared_positioning/DetermineAllanVarianceOfSingleAxisGyroscopeExample');
 
 %% ========== Read File ========== %%
-% Select file Z1
+% Select Z1 file
 PathZ1 = 'C:\Users\P66134111\Documents\code-class\Inertial-Survey-and-Navigation-System\Assignment2-git\data-sample\';
 FileZ1 = 'imu_pointer_z1.txt';
 % [FileZ1, PathZ1, ~] = uigetfile('*.txt', 'Please select Z1 file (.txt)');
 Z1 = load([PathZ1, FileZ1]);
 
-% Select file Z2
+% Select Z2 file
 PathZ2 = 'C:\Users\P66134111\Documents\code-class\Inertial-Survey-and-Navigation-System\Assignment2-git\data-sample\';
 FileZ2 = 'imu_pointer_z2.txt';
 % [FileZ2, PathZ2, ~] = uigetfile('*.txt', 'Please select Z2 file (.txt)');
 Z2 = load([PathZ2, FileZ2]);
 
-% Select file Allen
+% Select Allen file
 PathAllen = 'C:\Users\P66134111\Documents\NCKU-Data\112-2\Inertial-Survey-and-Navigation-System\assignment\Assignment2-class\Sample\Assignment2\';
 FileAllen = 'imu_pointer_Allan.txt';
 % [FileAllen, PathAllen, ~] = uigetfile('*.txt', 'Please select Allen file (.txt)');
 Allen = load([PathAllen, FileAllen]);
 
 %% ========== Make Scatter Plot ========== %%
-% % Z1 scatter
-% helperScatterPlotGyro(Z1(1:25979, [1 2 3 4]), [OutputFolder, '\','Z1-Gyro']);
-% helperScatterPlotAcce(Z1(1:25979, [1 5 6 7]), [OutputFolder, '\','Z1-Acce']);
-% 
-% % Z2 scatter
-% helperScatterPlotGyro(Z2(1:13580, [1 2 3 4]), [OutputFolder, '\','Z2-Gyro']);
-% helperScatterPlotAcce(Z2(1:13580, [1 5 6 7]), [OutputFolder, '\','Z2-Acce']);
+% Z1 scatter
+helperScatterPlotGyro(Z1(1:25979, [1 2 3 4]), [OutputFolder, '\','Z1-Gyro']);
+helperScatterPlotAcce(Z1(1:25979, [1 5 6 7]), [OutputFolder, '\','Z1-Acce']);
+
+% Z2 scatter
+helperScatterPlotGyro(Z2(1:13580, [1 2 3 4]), [OutputFolder, '\','Z2-Gyro']);
+helperScatterPlotAcce(Z2(1:13580, [1 5 6 7]), [OutputFolder, '\','Z2-Acce']);
+
+% Z1 scatter of Z-Axis
+helperScatterPlotGyro(Z1(1:25979, [1 4]), [OutputFolder, '\','Z1-Gyro-z-axis']);
+helperScatterPlotAcce(Z1(1:25979, [1 7]), [OutputFolder, '\','Z1-Acce-z-axis']);
+
+% Z2 scatter of Z-Axis
+helperScatterPlotGyro(Z2(1:13580, [1 4]), [OutputFolder, '\','Z2-Gyro-z-axis']);
+helperScatterPlotAcce(Z2(1:13580, [1 7]), [OutputFolder, '\','Z2-Acce-z-axis']);
 
 %% ========== Make Allan Plot ========== %%
-[avar,tau] = allanvar(Allen(:, 2), 'octave', 50);
-
-loglog(tau,avar);
-xlabel('\tau');
-ylabel('\sigma^2(\tau)');
-title('Allan Variance');
-grid on;
+% [avar,tau] = allanvar(Allen(:, 2), 'octave', 50);
+% 
+% loglog(tau,avar);
+% xlabel('\tau');s
+% ylabel('\sigma^2(\tau)');
+% title('Allan Variance');
+% grid on;
--- Inertial-Survey-and-Navigation-System/Assignment2-git/helperScatterPlotAcce.m
+++ Inertial-Survey-and-Navigation-System/Assignment2-git/helperScatterPlotAcce.m
@@ -1,13 +1,19 @@
 function [] = helperScatterPlotAcce(IMUData, OutputDir)
     scatter(IMUData(:, 1), IMUData(:, 2: end), 'filled');
-    title('Accelerometer and Time', 'FontSize', 16);
-    xlabel("time (s)", 'FontSize', 14);
-    ylabel("Acce value (m/s^2)", 'FontSize', 14);
-    legend('Acce-X', 'Acce-Y', 'Acce-Z', 'FontSize', 14);
+    title('Accelerometer and Time', 'FontSize', 32);
+    xlabel("time (s)", 'FontSize', 24);
+    ylabel("Acce value (m/s^2)", 'FontSize', 24);
+
+    if size(IMUData, 2) == 2
+        legend('Acce-Z', 'FontSize', 24);
+    else
+        legend('Acce-X', 'Acce-Y', 'Acce-Z', 'FontSize', 24);
+    end
 
     Pix_SS = get(0, 'screensize');
     width = 1750; height = 875;
     set(gcf, 'position', [(Pix_SS(3)-width)/2, (Pix_SS(4)-height)/2, width, height]);
-    saveas(gcf, OutputDir);
+    % saveas(gcf, [OutputDir, '.fig']);
+    saveas(gcf, [OutputDir, '.png']);
 end
 
