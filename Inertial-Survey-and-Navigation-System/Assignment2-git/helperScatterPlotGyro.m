c5639c971adcf1fc874974347de9320379331c2e
chinchia120 <chinchia120@gmail.com>
2024-05-06 16:19:11 +0800

    update

--- Inertial-Survey-and-Navigation-System/Assignment2-git/helperScatterPlotGyro.m
+++ Inertial-Survey-and-Navigation-System/Assignment2-git/helperScatterPlotGyro.m
@@ -3,7 +3,11 @@ function [] = helperScatterPlotGyro(IMUData, OutputDir)
     title('Gyroscope and Time', 'FontSize', 16);
     xlabel("time (s)", 'FontSize', 14);
     ylabel("Gyro value (rad/s)", 'FontSize', 14);
-    legend('Gyro-x', 'Gyro-y', 'Gyro-z', 'FontSize', 14);
+    legend('Gyro-X', 'Gyro-Y', 'Gyro-Z', 'FontSize', 14);
+
+    Pix_SS = get(0, 'screensize');
+    width = 1750; height = 875;
+    set(gcf, 'position', [(Pix_SS(3)-width)/2, (Pix_SS(4)-height)/2, width, height]);
     saveas(gcf, OutputDir);
 end
 
