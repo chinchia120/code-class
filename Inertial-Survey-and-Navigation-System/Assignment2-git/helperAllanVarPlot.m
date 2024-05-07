63f504a5e79dea53532c2a5b8cce25f70e50a348
chinchia120 <chinchia120@gmail.com>
2024-05-07 16:30:23 +0800

    update

---
+++ Inertial-Survey-and-Navigation-System/Assignment2-git/helperAllanVarPlot.m
@@ -0,0 +1,15 @@
+function [] = helperAllanVarPlot(AllanData, type, OutputDir)
+    [avar,tau] = allanvar(AllanData, 'octave', 50);
+    loglog(tau, avar);
+    title(['Allan Variance ', type], 'FontSize', 32);
+    xlabel('\tau', 'FontSize', 24);
+    ylabel('\sigma^2(\tau)', 'FontSize', 24);
+    grid on;
+
+    Pix_SS = get(0, 'screensize');
+    width = 1750; height = 875;
+    set(gcf, 'position', [(Pix_SS(3)-width)/2, (Pix_SS(4)-height)/2, width, height]);
+    % saveas(gcf, [OutputDir, '.fig']);
+    saveas(gcf, [OutputDir, '.png']);
+end
+
