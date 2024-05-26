e37160619f31f3c6045691298451cff8fa57425c
chinchia120 <chinchia120@gmail.com>
2024-05-27 07:03:38 +0800

    update

---
+++ Electronic-Navigation/homework3-git/homework3.m
@@ -0,0 +1,9 @@
+%% ========== Setup ========== %%
+clc;
+clear;
+close all;
+
+%% ========== Open Example ========== %%
+% openExample('satcom/GPSWaveformGenerationExample');
+% openExample('satcom/GPSL1CWaveformGenerationExample');
+
