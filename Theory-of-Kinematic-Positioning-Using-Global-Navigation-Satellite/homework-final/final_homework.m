8d2a69ef55a8a1c06881b4d5dbf88d67950f0587
chinchia120 <chinchia120@gmail.com>
2024-06-20 21:11:29 +0800

    update

--- Theory-of-Kinematic-Positioning-Using-Global-Navigation-Satellite/homework-final/final_homework.m
+++ Theory-of-Kinematic-Positioning-Using-Global-Navigation-Satellite/homework-final/final_homework.m
@@ -3,3 +3,17 @@ clc;
 clear;
 close all;
 
+%% ========== Select Input Folder ========== %%
+PathNameInput = uigetdir(addpath(genpath(pwd)), 'Please Select Input .FRS Folder.');
+FileList = dir(fullfile(PathNameInput, '*.FRS'));
+
+%% ========== Select Output Folder ========== %%
+PathNameOutput = uigetdir(addpath(genpath(pwd)), 'Please Select Output Folder.');
+
+%% ========== Load Data ========== %%
+RawData = [];
+
+for i = 1: numel(FileList)
+    file = readmatrix([FileList(i).folder,'\' FileList(i).name], 'FileType', 'text', 'ConsecutiveDelimitersRule', 'join', 'Range', 22);
+    RawData(size(RawData, 1)+1: size(RawData, 1)+size(file, 1), :) = file;
+end
