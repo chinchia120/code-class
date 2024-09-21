43e4aa2a04dc77b1a6b245ae1d311959b8c41c7e
chinchia120 <chinchia120@gmail.com>
2024-09-21 23:48:36 +0800

    update

---
+++ LeetCode/386_Lexicographical_Numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> lexicalOrder(int n)
+    {
+        vector<int> lexOrder;
+        while(1)
+        {   
+            n++;
+        }
+        return lexOrder;
+    }
+};
