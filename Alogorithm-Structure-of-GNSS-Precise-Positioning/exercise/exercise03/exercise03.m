8177a1bb9d00ebe58c3eb7c74ae269d4dadfacce
chinchia120 <chinchia120@gmail.com>
2024-10-17 23:23:42 +0800

    update

--- Alogorithm-Structure-of-GNSS-Precise-Positioning/exercise/exercise03/exercise03.m
+++ Alogorithm-Structure-of-GNSS-Precise-Positioning/exercise/exercise03/exercise03.m
@@ -2,6 +2,10 @@
 % ===== Setup
 clc; clear; close all;
 
+% ===== Create File
+file = fopen('exercise03_output.txt', 'w');
+fprintf(file, '%%%% ========== Exercise 3 ========== %%%%\n');
+
 % ===== Initial Value
 syms x y;
 
@@ -54,29 +58,56 @@ N1 = A1'*P1*A1;
 N2 = A2'*P2*A2;
 N3 = A3'*P3*A3;
 
-X = vpa((N1+N2+N3)\(A1'*P1*L1 + A2'*P2*L2 + A3'*P3*L3))
+X = vpa((N1+N2+N3)\(A1'*P1*L1 + A2'*P2*L2 + A3'*P3*L3));
+
+A = [A1; A2; A3];
+L = [L1; L2; L3];
+P = diag([1/3.0^2, 1/0.5^2, 1/0.5^2, 1/2^2, 1/1^2, 1/0.5^2, 1/1.0^2, 1/0.5^2, 1/0.3^2]);
+V = L-A*X;
+var1 = V'*P*V;
+
+fprintf(file, '%% ===== Question 1 ===== %%\n');
+fprintf(file, 'x = %.10f (m)\n', X(1));
+fprintf(file, 'y = %.10f (m)\n', X(2));
+fprintf(file, 'var = %.10f (m)\n\n', var1);
 
 %% ========== Question 2 ========== %%
 Ncond1 = Acond1'*Pcond1*Acond1;
-Xcond1 = vpa((N1+N2+N3+Ncond1)\(A1'*P1*L1 + A2'*P2*L2 + A3'*P3*L3 + Acond1'*Pcond1*Lcond1))
+Xcond1 = vpa((N1+N2+N3+Ncond1)\(A1'*P1*L1 + A2'*P2*L2 + A3'*P3*L3 + Acond1'*Pcond1*Lcond1));
 
 L = [L1; L2; L3; Lcond1];
 A = [A1; A2; A3; Acond1];
 P = diag([1/3.0^2, 1/0.5^2, 1/0.5^2, 1/2^2, 1/1^2, 1/0.5^2, 1/1.0^2, 1/0.5^2, 1/0.3^2, 1/0.00001^2, 1/0.00001^2]);
 Vcond1 = L-A*Xcond1;
 
-var = Vcond1'*P*Vcond1
+var2 = Vcond1'*P*Vcond1;
+
+fprintf(file, '%% ===== Question 2 ===== %%\n');
+fprintf(file, 'x = %.10f (m)\n', Xcond1(1));
+fprintf(file, 'y = %.10f (m)\n', Xcond1(2));
+fprintf(file, 'var = %.10f (m)\n\n', var2);
+
 %% ========== Question 3 ========== %%
 Ncond2 = Acond2'*Pcond2*Acond2;
-Xcond2 = vpa((N1+N2+N3+Ncond2)\(A1'*P1*L1 + A2'*P2*L2 + A3'*P3*L3 + Acond2'*Pcond2*Lcond2))
+Xcond2 = vpa((N1+N2+N3+Ncond2)\(A1'*P1*L1 + A2'*P2*L2 + A3'*P3*L3 + Acond2'*Pcond2*Lcond2));
 
 L = [L1; L2; L3; Lcond2];
 A = [A1; A2; A3; Acond2];
 P = diag([1/3.0^2, 1/0.5^2, 1/0.5^2, 1/2^2, 1/1^2, 1/0.5^2, 1/1.0^2, 1/0.5^2, 1/0.3^2, 1/0.00001^2, 1/0.00001^2]);
 Vcond2 = L-A*Xcond2;
 
-var = Vcond2'*P*Vcond2
+var3 = Vcond2'*P*Vcond2;
 
+fprintf(file, '%% ===== Question 3 ===== %%\n');
+fprintf(file, 'x = %.10f (m)\n', Xcond2(1));
+fprintf(file, 'y = %.10f (m)\n', Xcond2(2));
+fprintf(file, 'var = %.10f (m)\n\n', var3);
 
+fprintf(file, '%% ===== Analysis ===== %%\n');
+fprintf(file, ['In the Question 2 and Question 3, they have constrain conditions in x and y. Both of\n' ...
+               'the conditions have very large weighted. Thus, the variable x and y are almost equal\n' ...
+               'to the condition, as the answer shown in Question 2 and 3. In the variances, Question 3\n' ...
+               'is many times bigger than Question 2, because constrain condition isn''t reasonable in\n' ...
+               'Question 3. If fixed the variables, the equation will present large error in this case.']);
 
-
+fclose(file);
