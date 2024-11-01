b8dc31bdcd2ba13db2b173e517e25d6546fc614c
chinchia120 <chinchia120@gmail.com>
2024-11-01 09:45:11 +0800

    upload SG_HW1 Initial Value

--- Satellite-Geodesy/homework1/SG_HW1.m
+++ Satellite-Geodesy/homework1/SG_HW1.m
@@ -10,6 +10,15 @@ if ~exist(OutputFolder, 'dir'); mkdir(OutputFolder); end
 file = fopen([OutputFolder '/SG_HW1_output.txt'], 'w');
 
 % ===== Initial Value
+x = 20952940.878790;        % m
+y = 11599221.8785432;       % m
+z = 11802653.3351916;       % m
+vx = -2338.53157760597;     % m/s
+vy = 1501.60308576919;      % m/s
+vz = 2669.76244747639;      % m/s
+
+GM = 3.986005 * 10^14       % m^3/s^2
+ae = 6378137;               % m
 
 %% ========== Q1 ========== %% 
 
