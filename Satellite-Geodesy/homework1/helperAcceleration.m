99844e16bf5254899be0d1b55ccfd30cdbd72ca5
chinchia120 <chinchia120@gmail.com>
2024-11-05 00:20:29 +0800

    update SG-HW1

--- Satellite-Geodesy/homework1/helperAcceleration.m
+++ Satellite-Geodesy/homework1/helperAcceleration.m
@@ -8,7 +8,7 @@ ae = 6378137;               % m
 r = sqrt(x^2 + y^2 + z^2);
 
 % ===== Calculate Acceleration
-ax = GM*x/r^3 * (1 - J2 * 3/2 * (ae/r)^2 * (5*z^2/r^2 - 1));
+ax = -GM*x/r^3 * (1 - J2 * 3/2 * (ae/r)^2 * (5*z^2/r^2 - 1));
 ay = y/x * ax;
 az = -GM*z/r^3 * (1 + J2 * 3/2 * (ae/r)^2 * (3 - 5*z^2/r^2));
 
