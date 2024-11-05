7cdc6f0f43c71e2a8706cf0c6c53318c3bada5b8
chinchia120 <chinchia120@gmail.com>
2024-11-05 15:23:56 +0800

    update SG-HW1 Output

--- Satellite-Geodesy/homework1/SG_HW1.m
+++ Satellite-Geodesy/homework1/SG_HW1.m
@@ -41,7 +41,7 @@ fclose(file);
 file = fopen([OutputFolder '/SG_HW1_J2.txt'], 'w');
 
 % ===== Calculate State of Satellite
-fprintf(file, '%%%% ========================= Satellite''s States Using Keplerian Motion ========================= %%%%\n');
+fprintf(file, '%%%% ================== Satellite''s States Using Keplerian Motion and J2 Effect ================== %%%%\n');
 fprintf(file, 'Hour\t        X\t                Y\t                Z\t           X_dot\t       Y_dot\t       Z_dot\n');
 fprintf(file, ' %2d\t %16.6f\t %16.6f\t %16.6f\t %12.6f\t %12.6f\t %12.6f\n', 0, X(1, :));
 
