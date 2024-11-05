e11c814bc90831b2b55d84790c7e1378e15b9ce6
chinchia120 <chinchia120@gmail.com>
2024-11-05 14:50:48 +0800

    update SG-HW1 Output

--- Satellite-Geodesy/homework1/SG_HW1.m
+++ Satellite-Geodesy/homework1/SG_HW1.m
@@ -22,9 +22,9 @@ X(1, :) = [x y z vx vy vz];
 file = fopen([OutputFolder '/SG_HW1_Keplerian.txt'], 'w');
 
 % ===== Calculate State of Satellite
-fprintf(file, '%%%% =============================== Satellite''s State Using Keplerian Motion =============================== %%%%\n');
-fprintf(file, 'Hour\t     X\t                 Y\t                 Z\t             X_dot\t         Y_dot\t        Z_dot\n');
-fprintf(file, ' %2d\t %16.6f\t %16.6f\t %16.6f\t %12.6f\t %12.6f\t %12.6f\n', 0, X(1, :));
+fprintf(file, '%%%% ========================= Satellite''s States Using Keplerian Motion ========================= %%%%\n');
+fprintf(file, 'Hour\t        X\t                Y\t                Z\t           X_dot\t       Y_dot\t       Z_dot\n');
+fprintf(file, '%3d\t %16.6f\t %16.6f\t %16.6f\t %12.6f\t %12.6f\t %12.6f\n', 0, X(1, :));
 
 for i = 2: 60*60*24+1
     X(i, :) = helperRK4(X(i-1, :), 1, 1);
@@ -41,8 +41,8 @@ fclose(file);
 file = fopen([OutputFolder '/SG_HW1_J2.txt'], 'w');
 
 % ===== Calculate State of Satellite
-fprintf(file, '%%%% ======================== Satellite''s State Using Keplerian Motion and J2 Effect ======================== %%%%\n');
-fprintf(file, 'Hour\t     X\t                 Y\t                 Z\t             X_dot\t         Y_dot\t        Z_dot\n');
+fprintf(file, '%%%% ========================= Satellite''s States Using Keplerian Motion ========================= %%%%\n');
+fprintf(file, 'Hour\t        X\t                Y\t                Z\t           X_dot\t       Y_dot\t       Z_dot\n');
 fprintf(file, ' %2d\t %16.6f\t %16.6f\t %16.6f\t %12.6f\t %12.6f\t %12.6f\n', 0, X(1, :));
 
 for i = 2: 60*60*24+1
