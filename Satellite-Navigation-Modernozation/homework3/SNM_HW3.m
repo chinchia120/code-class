c8eb90450b571ca81d705f8b25b24bd45e7c8218
chinchia120 <chinchia120@gmail.com>
2024-11-27 16:10:44 +0800

    remove .DS_Store

--- Satellite-Navigation-Modernozation/homework3/SNM_HW3.m
+++ Satellite-Navigation-Modernozation/homework3/SNM_HW3.m
@@ -6,9 +6,6 @@ clc; clear; close all;
 OutputFolder = sprintf('OutputFigure');
 if ~exist(OutputFolder, 'dir'); mkdir(OutputFolder); end
 
-% ===== Create Output File
-% file = fopen([OutputFolder '/SNM_HW2_output.txt'], 'w');
-
 % ===== Initial Value
 mat = load('signal.mat');
 signal_1 = mat.signal_1;
