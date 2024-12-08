0c3ef09e72bbb924a6b18b65ecd65e26e0e20c3b
chinchia120 <chinchia120@gmail.com>
2024-12-08 23:19:03 +0800

    update SNM-HW4

--- Satellite-Navigation-Modernozation/homework4/SNM_HW4.m
+++ Satellite-Navigation-Modernozation/homework4/SNM_HW4.m
@@ -3,6 +3,9 @@
 clc; clear; close all;
 format long;
 
+% ===== Reference IS-GPS-200M
+% https://www.gps.gov/technical/icwg/IS-GPS-200M.pdf
+
 % ===== Initial Value
 c = 299792458;              % the speed of light (m/s)
 wedot = 7.2921151467 * 10^-5;   % earth rotation rate
@@ -36,6 +39,7 @@ d_Ek = 100;
 while min(abs(d_Ek)) > 10^-12
     tmp = Ek;
     Ek = Ek + ((Mk-Ek+eph.e.*sin(Ek)) ./ (1-eph.e.*cos(Ek)));
+    % Ek = Mk + eph.e.*sin(Ek);
 
     d_Ek = Ek-tmp;
 end
@@ -77,26 +81,29 @@ d_tsv = eph.af0 + eph.af1.*tk + eph.af2.*tk.^2 + d_tr;
 
 %% ========== Tropospheric Delay Correction ========== %%
 % ===== Initial Position
-% init_lla = wgsxyz2lla(init_wgs84_xyz);
+init_lla = wgsxyz2lla(init_wgs84_xyz);
 
 % ===== Calculate the Vector of Satellite to Receiver in ENU
-% sv_enu = zeros(length(rcvr.svid), 3);
-% for i = 1: length(sv_enu)
-%     sv_enu(i, :) = wgsxyz2enu(wgs84_xyz(i)-init_wgs84_xyz, init_lla(1), init_lla(2), init_lla(3));
-% end
+sv_enu = zeros(length(rcvr.svid), 3);
+for i = 1: length(sv_enu)
+    sv_enu(i, :) = wgsxyz2enu(wgs84_xyz(i)-init_wgs84_xyz, init_lla(1), init_lla(2), init_lla(3));
+end
 
 % ===== Elevation Angle
-% elevation_angle = atan2(sv_enu(:, 3), norm(sv_enu(:, 1:2)));
+elevation_angle = atan2(sv_enu(:, 3), norm(sv_enu(:, 1:2)));
 
 % ===== Zenith Angle
-% z = pi - elevation_angle;
+z = pi - elevation_angle;
 
 % ===== Standard Temperature and Pressure
 P0 = 1013.25; % Partial Pressure of Dry Air (mbars)
 T0 = 273.15; % Temperature (K)
 e0 = 6; % Partial Pressure of Water Vapor (mbars)
 
-% ===== Tropospheric Delay
+% ===== Tropospheric Delay - Saastamoinen Model
+tro = 0.002277 ./ cos(z) .* (P0+((1255/T0)+0.05)*e0-tand(z).^2);
+
+% ===== Tropospheric Delay - Hopfield Model
 tro = 77.6*10^-6*P0*43/(5*T0) + 0.373*e0*12/(5*T0^2);
 
 %% ========== Earth Rotation Correction ========== %%
