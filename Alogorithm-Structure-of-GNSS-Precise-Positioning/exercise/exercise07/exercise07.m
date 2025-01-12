17bf661887fbadbd6783619d97a83702c8077a96
chinchia120 <chinchia120@gmail.com>
2025-01-12 15:08:32 +0800

    update GNSS Exercise 06

---
+++ Alogorithm-Structure-of-GNSS-Precise-Positioning/exercise/exercise07/exercise07.m
@@ -0,0 +1,11 @@
+%% ========== Setup ========== %%
+% ===== Setup
+clc; clear; close all;
+
+% ===== Creat Output Folder
+OutputFolder = sprintf('OutputFigure');
+if ~exist(OutputFolder, 'dir'); mkdir(OutputFolder); end
+
+% ===== Initial Value
+GPSTime = load()
+
