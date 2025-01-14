c57ee5ec4a97fa9de6341359287152ec416aaf28
chinchia120 <chinchia120@gmail.com>
2025-01-14 23:59:02 +0800

    update GNSS Exercise 12

--- Alogorithm-Structure-of-GNSS-Precise-Positioning/exercise/exercise12/exercise12.m
+++ Alogorithm-Structure-of-GNSS-Precise-Positioning/exercise/exercise12/exercise12.m
@@ -7,6 +7,8 @@ OutputFolder = sprintf('OutputFigure');
 if ~exist(OutputFolder, 'dir'); mkdir(OutputFolder); end
 
 % ===== Initial Value
+CKSV = [-2956619.406; 5075902.161; 2476625.471];
+
 ACOM = struct2array(load('A_Matrix.mat'));
 LCOM = struct2array(load('L_Vector.mat'));
 WeightMatrix = struct2array(load('WeightMatrix.mat'));
