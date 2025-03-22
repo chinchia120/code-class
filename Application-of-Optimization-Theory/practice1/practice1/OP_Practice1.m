ac4e6f94558e1ba4f3e610fff1cc4cbe7b0ffa66
chinchia120 <chinchia120@gmail.com>
2025-03-22 15:15:39 +0800

    update OP Practice1 Q1

--- Application-of-Optimization-Theory/practice1/practice1/OP_Practice1.m
+++ Application-of-Optimization-Theory/practice1/practice1/OP_Practice1.m
@@ -2,10 +2,30 @@
 % ===== Setup
 clc; clear; close all;
 
+% ===== Output Data
+OutputFolder = sprintf('OutputFigure');
+if ~exist(OutputFolder, 'dir'); mkdir(OutputFolder); end
+
 %% ========== Question 1 ========== %%
-func1 = @(x,y) (x^2 + y^2 + 3*x - 4*y + 6);
-initial = [0, 0];
+% ===== Initial Value
 ub = [10, 10];
 lb = [-10, -10];
+x0 = [0, 0];
+
+function f = objectiveFcn1(optimInput)
+    x = optimInput(1);
+    y = optimInput(2);
+    f = x^2 + y^2 + 3*x - 4*y + 6;
+end
+
+% ===== Solve Solution using Optimization Tool
+option1 = optimoptions("fmincon","Display","off");
+[solution,objectiveValue] = fmincon(@objectiveFcn1,x0,[],[],[],[],lb,ub,[],option1);
+
+% ===== Plot level sets
+plotLevelSets(lb, ub, @objectiveFcn1, [OutputFolder '/LevelSets_1']);
+
+% ===== Plot Solution
+plotSolution(lb, ub, @objectiveFcn1, solution, [OutputFolder '/Soution_1']);
 
 %% ========== Question 2 ========== %%
---
+++ Application-of-Optimization-Theory/practice1/practice1/plotLevelSets.m
@@ -0,0 +1,30 @@
+function plotLevelSets(lb, ub, objFcn, OutName)
+%% ========== Setup ========== %%
+figure; clf;
+
+%% ========== Meshgrid ========== %%
+[X, Y] = meshgrid(lb(1):0.1:ub(1), lb(2):0.1:ub(2));
+    
+%% ========== Objective Function ========== %%
+Z = zeros(size(X));
+for i = 1:size(X,1)
+    for j = 1:size(X,2)
+        Z(i,j) = objFcn([X(i,j), Y(i,j)]);
+    end
+end
+
+%% ========== Plot Level Set ========== %%
+contour(X, Y, Z, 100);
+
+%% ========== Plot Config ========== %%
+colorbar;
+xlabel('x');
+ylabel('y');
+title('Level Sets of Objective Function');
+legend('Level Sets');
+grid on;
+
+%% ========== Save Figure ========== %%
+saveas(gcf, OutName, 'png');
+
+end
