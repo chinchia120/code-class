8372b2ecf36c389c154f516a5c9ecba1303e83ac
chinchia120 <chinchia120@gmail.com>
2025-03-29 17:31:30 +0800

    upload CD Homework1 RawTimeSeries

--- Application-of-Geodetic-Techniques-to-Crustal-Deformation/homework1/CD_homework1.m
+++ Application-of-Geodetic-Techniques-to-Crustal-Deformation/homework1/CD_homework1.m
@@ -1,3 +1,20 @@
 %% ========== Setup ========== %%
 % ===== Setup
 clc; clear; close all;
+
+%% ========== Read Dataset ========== %%
+FolderPath = uigetdir(pwd, 'Select Folder');
+Files = dir(fullfile(FolderPath, '*.gmt'));
+
+%% ========== Creat Output Folder ========== %%
+OutputFolder = sprintf('OutputFigure');
+if ~exist(OutputFolder, 'dir'); mkdir(OutputFolder); end
+
+%% ========== Plot Raw Time Series ========== %%
+for sta = 1: length(Files)/3
+    staE = [FolderPath sprintf('/ts_CK%02d_e.gmt', sta)];
+    staN = [FolderPath sprintf('/ts_CK%02d_n.gmt', sta)];
+    staU = [FolderPath sprintf('/ts_CK%02d_u.gmt', sta)];
+
+    plotTimeSeries(staE, staN, staU, sprintf('CK%02d', sta), [OutputFolder sprintf('/CK%02d', sta)]);
+end
