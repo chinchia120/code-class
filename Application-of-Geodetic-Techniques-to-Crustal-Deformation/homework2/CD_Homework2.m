de0364e1ce95e63d080b26ca4a418e3bb0475ea8
chinchia120 <chinchia120@gmail.com>
2025-04-07 18:19:25 +0400

    update Homework Figure

--- Application-of-Geodetic-Techniques-to-Crustal-Deformation/homework2/CD_Homework2.m
+++ Application-of-Geodetic-Techniques-to-Crustal-Deformation/homework2/CD_Homework2.m
@@ -18,6 +18,7 @@ StationData = StationData(:, 1:2);
 %% ========== Read Strain Data ========== %%
 StrainFile = fopen([FolderPath '/strain.gmt']);
 StrainData = cell2mat(textscan(StrainFile, '%f %f %f %f %f', 'Delimiter', '\t', 'MultipleDelimsAsOne', true));
+StrainData = StrainData(:, 1:4);
 fclose(StrainFile);
 
 %% ========== Read Shear Data ========== %%
@@ -26,9 +27,9 @@ ShearData = cell2mat(textscan(ShearFile, '%f %f %f %f', 'Delimiter', '\t', 'Mult
 ShearData = ShearData(1:2:end, :);
 fclose(ShearFile);
 
-%% ========== Read Shear Angle Data ========== %%
+%% ========== Read Angle Data ========== %%
 AngleFile = fopen([FolderPath '/strain.out']);
-AngleData = zeros(size(StrainData, 1), 1);
+AngleData = zeros(size(StrainData, 1), 2);
 cnt = 0;
 while ~feof(AngleFile)
     cnt = cnt + 1;
@@ -36,13 +37,14 @@ while ~feof(AngleFile)
     anglespt = strsplit(fgetl(AngleFile), ' ');
     if cnt == 1; continue; end
 
-    AngleData(cnt-1, :) = [str2double(anglespt(18))];
+    AngleData(cnt-1, :) = [str2double(anglespt(10)) str2double(anglespt(18))];
 end
 fclose(AngleFile);
 
 %% ========== Plot Principal Strain Rate ========== %%
-plotStrain(StationData, StrainData, [OutputFolder '/Strain']);
+StrainAngleData = [StrainData AngleData(:, 1)];
+plotStrain(StationData, StrainAngleData, [OutputFolder '/Strain']);
 
 %% ========== Plot Shear Strain Rate ========== %%
-ShearAngleData = [ShearData AngleData];
+ShearAngleData = [ShearData AngleData(:, 2)];
 plotShear(StationData, ShearAngleData, [OutputFolder '/Shear']);
