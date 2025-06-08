d4a3b83366a92e0604ae15fb2977c7a60d06ee84
Chin-Chia Hsu <chinchia120@gmail.com>
2025-06-09 03:10:53 +0800

    new OP Computer Project Figure

--- Application-of-Optimization-Theory/practice/practice2/source code/PlotOutFigureAll.m
+++ Application-of-Optimization-Theory/practice/practice2/source code/PlotOutFigureAll.m
@@ -1,7 +1,11 @@
-function PlotOutFigure(f_plot, ub, lb, loss, points_array, values_array, method, outname)
-%% ========== Convergence Curve ========== %%
+function PlotOutFigureAll(f_plot, ub, lb, loss, points_array, values_array, method, outname)
+%% ========== Setup ========== %%
 % ===== Setup
 figure;
+
+%% ========== Convergence Curve ========== %%
+% ===== Setup
+subplot(2, 2, 1);
 
 % ===== Plot Convergence Curve
 x = linspace(0, length(values_array)-1, length(values_array));
@@ -9,17 +13,14 @@ plot(x, values_array, 'LineWidth', 2);
 
 % ===== Plot Config
 grid minor;
-title([method ' Convergence Curve']);
+title('Convergence Curve');
 xlabel('Iteration');
 ylabel('Value');
 xlim([x(1) x(end)]);
 
-% ===== Save Plot
-saveas(gcf, [outname '_Convergence.png']);
-
 %% ========== Gradient Curve ========== %%
 % ===== Setup
-figure;
+subplot(2, 2, 2);
 
 % ===== Plot Gradient Curve
 x = linspace(1, length(loss), length(loss));
@@ -27,17 +28,14 @@ plot(x, loss, 'LineWidth', 2);
 
 % ===== Plot Config
 grid minor;
-title([method ' Gradient Curve']);
+title('Gradient Curve');
 xlabel('Iteration');
 ylabel('Difference');
 xlim([x(1) x(end)]);
 
-% ===== Save Plot
-saveas(gcf, [outname '_Gradient.png']);
-
-%% ========== Level Set with Path ========== %%
+%% ========== Level Set and Path ========== %%
 % ===== Setup
-figure;
+subplot(2, 2, 3);
 
 % ===== Plot Level Set
 x = linspace(ub(1), lb(1), 100);
@@ -52,18 +50,15 @@ plot(points_array(:, 1), points_array(:, 2), '-o', 'Color', 'r', 'LineWidth', 2)
 hold off;
 
 % ===== Plot Config
-title([method ' Level Set with Path']);
+title('Level Set with Path');
 xlabel('x1');
 ylabel('x2');
 
-% ===== Save Plot
-saveas(gcf, [outname '_LevelSet.png']);
-
-%% ========== Surface with Path ========== %%
+%% ========== Surface and Path ========== %%
 % ===== Setup
-figure;
+subplot(2, 2, 4);
 
-% ===== Plot Level Set
+% ===== Plot Surface
 x = linspace(ub(1), lb(1), 100);
 y = linspace(ub(2), lb(2), 100);
 [xx, yy] = meshgrid(x, y);
@@ -76,12 +71,14 @@ plot3(points_array(:, 1), points_array(:, 2), values_array, '-o', 'Color', 'r', 
 hold off;
 
 % ===== Plot Config
-title([method ' Surface with Path']);
+title('Surface with Path');
 xlabel('x1');
 ylabel('x2');
 zlabel('f(x1, x2)')
 
-% ===== Save Plot
-saveas(gcf, [outname '_Surface.png']);
+% ===== Adjust layout and save
+sgtitle(sprintf('%s [%.4f, %.4f]', method, points_array(1, :)), 'FontSize', 14, 'FontWeight', 'bold');
+set(gcf, 'Color', 'white');
+saveas(gcf, [outname '_All.png']);
 
 end
