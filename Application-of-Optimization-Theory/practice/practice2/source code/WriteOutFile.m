d43d39c39476df96705f54b70306725c4fb5a6b3
Chin-Chia Hsu <chinchia120@gmail.com>
2025-06-10 01:01:14 +0800

    new OP Computer Project Figure

--- Application-of-Optimization-Theory/practice/practice2/source code/WriteOutFile.m
+++ Application-of-Optimization-Theory/practice/practice2/source code/WriteOutFile.m
@@ -1,11 +1,11 @@
-function [] = WriteOutFile(point, output, time, iter, loss, points_array, values_array, method, outputfile)
+function [] = WriteOutFile(point, output, time, iter, points_array, method, outputfile)
 %% ========== Open OutputFile ========== %%
 fid = fopen(outputfile, 'a+');
 
 %% ========== Output Data ========== %%
 fprintf(fid, '%% ========== %s ========== %% \r\n', method);
-fprintf(fid, 'Initial Value = [%5.1f, %5.1f]\r\n', points_array(1, :));
-fprintf(fid, 'Optimal Point = [%5.1f, %5.1f]\r\n', point);
+fprintf(fid, 'Initial Value = [%8.4f, %8.4f]\r\n', points_array(1, :));
+fprintf(fid, 'Optimal Point = [%8.4f, %8.4f]\r\n', point);
 fprintf(fid, 'Optimal Value = %.4e\r\n', output);
 fprintf(fid, 'Iterations    = %d\r\n', iter);
 fprintf(fid, 'CPU Time      = %.4f (s)\r\n\r\n', time);
